2171f17c9360e4da7a17c413c784e0b38189dd7f
Willy Sly <122539997+WillySly@users.noreply.github.com>
2025-11-27 21:12:04 +0400

    test5

--- webgl/chompocalypse/Build/bc4adc65649d42d23be10da713620d61.loader.js
+++ webgl/chompocalypse/Build/bc4adc65649d42d23be10da713620d61.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 132556647,
+    wasmFileSize: 146581739,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
